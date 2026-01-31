d887fb8ddf4bbac592f28c390cf44ee0de315abe
Boso Mac <robertoingrao4@gmail.com>
2026-01-31 09:53:03 +0100

    selfwork oggeti 2

---
+++ main.js
@@ -0,0 +1,7 @@
+// Crea un oggetto agenda con una proprietà che comprenda una lista di contatti con un nome e un numero di telefono, ed abbia diverse funzionalità tra cui:
+// mostrare tutti i contatti dell’agenda
+// mostrare un singolo contatto
+// eliminare un contatto dall’agenda
+// aggiungere un nuovo contatto
+// modificare un contatto esistente  
+
